9d22e9cab68c9431791239c7d132c7f751ffb281
cheshire cat <45429645+ermii42@users.noreply.github.com>
2022-11-20 16:01:28 +0300

    вроде правильно работающее цепное хеширование

--- Hash.h
+++ Hash.h
@@ -6,21 +6,23 @@
 const int c = 1; //c – константа, определяющая шаг перебора, линейного пробирования
 
 #include <iostream>
+
 using namespace std;
+
 #include <list>
 
 // структура записи
 struct typeitem {
-    int code=0;                 // ключ записи - артикул, шестиразрядное число
-    string name="-";            // название
-    string factory="-";         // завод изготовитель
+    int code = 0;                 // ключ записи - артикул, шестиразрядное число
+    string name = "-";            // название
+    string factory = "-";         // завод изготовитель
     int price = 0;              // цена
     string country = "-";       // страна (название)
     int record_number;          // порядковый номер записи
 
     typeitem *next = nullptr;   // указатель на следующую запись с таким же ключом
-    bool openORclose=true;      // свободна ли ячейка
-    bool deletedORnot=false;    // не удалялась ли ячейка
+    bool openORclose = true;      // свободна ли ячейка
+    bool deletedORnot = false;    // не удалялась ли ячейка
 
 };
 
@@ -33,14 +35,16 @@ struct HeshTable {
     void createHeshTable() {
         T = new typeitem[L];
         insertedcount = 0;
-        deletedcount=0;
+        deletedcount = 0;
     }
-    void Resize(int newL){  //увеличение размера таблицы
+
+    void Resize(int newL) {  //увеличение размера таблицы
         delete[] T;
         L = newL;
         T = new typeitem[L];
     }
-    void Delete(){
+
+    void Delete() {
         delete[] T;
     }
 };
@@ -51,20 +55,21 @@ int hesh(int code, int L) {
 }
 
 //вставка с рехешированием
-int insertInHeshTable(int code, string name, string factory, int price, string country, int record_number, HeshTable& t) {
+int
+insertInHeshTable(int code, string name, string factory, int price, string country, int record_number, HeshTable &t) {
 
     // рехеширование
-    if(float(t.insertedcount) / t.L >= 0.75){ // коэффициент нагрузки
+    if (float(t.insertedcount) / t.L >= 0.75) { // коэффициент нагрузки
         HeshTable T2;   // создание новой таблицы и увеличивание ее размера в сравнении с предыдущей вдвое
         T2.createHeshTable();
-        T2.Resize(t.L*2);
+        T2.Resize(t.L * 2);
         typeitem *nxt;
         // заполнение новой таблицы старыми значениями с учетом нового значения размера (рехеширую)
-        for(int i=0; i<t.L; i++){
+        for (int i = 0; i < t.L; i++) {
             nxt = &(t.T[i]);
-            while(nxt != nullptr){
+            while (nxt != nullptr) {
                 // добавляю только непустые неудаленные элементы
-                if(nxt->openORclose == false && nxt->deletedORnot==false){
+                if (nxt->openORclose == false && nxt->deletedORnot == false) {
                     insertInHeshTable(nxt->code, nxt->name, nxt->factory,
                                       nxt->price, nxt->country, nxt->record_number, T2);
                 }
@@ -83,63 +88,69 @@ int insertInHeshTable(int code, string name, string factory, int price, string c
     int i = hesh(code, t.L);
     typeitem *nxt = &(t.T[i]);
     //разрешение коллизии
-    if(nxt->next == nullptr && nxt->openORclose == true){
-        nxt->code=code; nxt->name=name; nxt->country=country;
-        nxt->factory=factory; nxt->price=price; nxt->record_number=record_number;
-        nxt->openORclose=false;
+    if (nxt->next == nullptr && nxt->openORclose == true) {
+        nxt->code = code;
+        nxt->name = name;
+        nxt->country = country;
+        nxt->factory = factory;
+        nxt->price = price;
+        nxt->record_number = record_number;
+        nxt->openORclose = false;
         return 0;
     }
     while (nxt->next != nullptr)
         nxt = nxt->next;
-    if (i < t.L)
-    {
-        typeitem* newElem = new typeitem();
+    if (i < t.L) {
+        typeitem *newElem = new typeitem();
 
-        newElem->code = code; newElem->name = name; newElem->factory=factory;
-        newElem->price=price; newElem->country=country; newElem->record_number = record_number;
+        newElem->code = code;
+        newElem->name = name;
+        newElem->factory = factory;
+        newElem->price = price;
+        newElem->country = country;
+        newElem->record_number = record_number;
         newElem->openORclose = false;
-        nxt->next=newElem;
+        nxt->next = newElem;
         t.insertedcount++;
         return 0;
-    }
-    else
+    } else
         return -1;
 }
 
 // вывод хеш-таблицы
-void outTable(HeshTable& t) {
+void outTable(HeshTable &t) {
     typeitem *nxt;
     for (int i = 0; i < t.L; i++) {
         nxt = &(t.T[i]);
-        while(nxt!= nullptr) {
-            cout << i << '\t' << nxt->code << "\t" << nxt->name << "\t" << nxt->factory<<"\t"
-            << nxt->price << "\t" << nxt->country << "\t" << nxt->openORclose << "  "
-            << nxt->deletedORnot << '\n';
+        while (nxt != nullptr) {
+            cout << i << '\t' << nxt->code << "\t" << nxt->name << "\t" << nxt->factory << "\t"
+                 << nxt->price << "\t" << nxt->country << "\t" << nxt->openORclose << "  "
+                 << nxt->deletedORnot << '\n';
             nxt = nxt->next;
         }
     }
 }
 
 // поиск
-int search(HeshTable& t, int code) {
+int search(HeshTable &t, int code) {
     int i = hesh(code, t.L);
     //ищем по кластеру
     int result;
     typeitem *nxt;
     nxt = &(t.T[i]);
 
-    while (nxt!= nullptr && ((nxt->openORclose == false && nxt->deletedORnot==false)
-                       || (nxt->openORclose == true && nxt->deletedORnot == true))
+    while (nxt != nullptr && ((nxt->openORclose == false && nxt->deletedORnot == false)
+                              || (nxt->openORclose == true && nxt->deletedORnot == true))
            && nxt->code != code)
-        nxt =nxt->next;
-    if (not (nxt->openORclose == true && nxt->deletedORnot == false)) {
+        nxt = nxt->next;
+    if (not(nxt->openORclose == true && nxt->deletedORnot == false)) {
         result = i;
     }
     return result;
 }
 
 //удаление
-int deletedFromHeshTable(HeshTable& t, int code) {
+int deletedFromHeshTable(HeshTable &t, int code) {
     int i;
     i = search(t, code);
     if (i == -1) return 1;//нет такой записи в таблице
